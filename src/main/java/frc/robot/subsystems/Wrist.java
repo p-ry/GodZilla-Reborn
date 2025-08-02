e302de5ab0d6ba1d41732dd9ecf8a23d216cbb36
FRC4336 <FRC4336@gmail.com>
2025-08-02 06:28:02 -0500

    Update Wrist.java

--- src/main/java/frc/robot/subsystems/Wrist.java
+++ src/main/java/frc/robot/subsystems/Wrist.java
@@ -1,149 +1,133 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot.subsystems;
 
 import edu.wpi.first.util.sendable.Sendable;
 import edu.wpi.first.util.sendable.SendableBuilder;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-import org.opencv.core.Mat;
-
-import com.ctre.phoenix6.configs.CommutationConfigs;
-import com.ctre.phoenix6.configs.MotorOutputConfigs;
 import com.ctre.phoenix6.configs.Slot0Configs;
-import com.ctre.phoenix6.configs.SoftwareLimitSwitchConfigs;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
-import com.ctre.phoenix6.configs.TalonFXConfiguration;
-import com.ctre.phoenix6.configs.TalonFXConfigurator;
 import com.ctre.phoenix6.controls.MotionMagicVoltage;
-import com.ctre.phoenix6.controls.PositionDutyCycle;
 import com.ctre.phoenix6.hardware.TalonFX;
-import com.ctre.phoenix6.signals.MotorArrangementValue;
 import com.ctre.phoenix6.signals.NeutralModeValue;
+import com.ctre.phoenix6.configs.TalonFXConfigurator;
 
-//import com.revrobotics.servohub.ServoHub.ResetMode;
-import com.revrobotics.spark.SparkClosedLoopController;
+import edu.wpi.first.math.MathUtil;
 
 public class Wrist extends SubsystemBase implements Sendable {
 
-  TalonFX wrist;
+  private final TalonFX wrist;
+  private final TalonFXConfigurator wristConfigurator;
+  private final MotionMagicVoltage motionMagicRequest;
 
+  private final TalonFXConfiguration wristConfigs;
+  private final Slot0Configs pidConfigs;
 
-  TalonFXConfigurator wristConfigurator;
-  PositionDutyCycle wristController;
-  // CommutationConfigs commutationConfigs;
+  private double requestedPosition = 0;
+  private double newPosition = 0;
+  private boolean atPosition = false;
 
-  TalonFXConfiguration wristConfigs;
-  double requestedPosition,newPosition;
-  boolean atPosition;
+  private double cachedPos = 0;
 
-  public double kP, kI, kD, kIz, kFF, kMaxOutput, kMinOutput, maxRPM, maxVel, minVel, maxAcc, allowedErr;
-  public double i, d, ff, aFF;
-  private Slot0Configs pidConfigs = new Slot0Configs();
-
-  /** Creates a new Wrist. */
+  /** Creates a new Wrist subsystem. */
   public Wrist() {
-
-    wrist = new TalonFX(36,"Canivore2");
+    wrist = new TalonFX(36, "Canivore2");
     wristConfigurator = wrist.getConfigurator();
-
-    wristController = new PositionDutyCycle(0);
+    motionMagicRequest = new MotionMagicVoltage(0);
     wristConfigs = new TalonFXConfiguration();
     pidConfigs = wristConfigs.Slot0;
+
+    // PID tuning
     pidConfigs.kP = 0.05;
+    pidConfigs.kI = 0.0;
+    pidConfigs.kD = 0.0;
+    pidConfigs.kV = 0.0;
+
+    // Motion Magic profile
+    wristConfigs.MotionMagic.MotionMagicCruiseVelocity = 25.0;
+    wristConfigs.MotionMagic.MotionMagicAcceleration = 80.0;
+
+    // Limits and safety
     wristConfigs.ClosedLoopGeneral.ContinuousWrap = false;
     wristConfigs.SoftwareLimitSwitch.ForwardSoftLimitEnable = true;
     wristConfigs.SoftwareLimitSwitch.ReverseSoftLimitEnable = true;
     wristConfigs.SoftwareLimitSwitch.ForwardSoftLimitThreshold = 10.0;
-    wristConfigs.SoftwareLimitSwitch.ReverseSoftLimitThreshold= -0.1;
-    wristConfigs.MotorOutput.NeutralMode=NeutralModeValue.Brake;
-    
-    wrist.getConfigurator().apply(wristConfigs);
-    
-    //wristConfigs.Commutation.MotorArrangement = MotorArrangementValue.Minion_JST;
-    // wristConfigs.ExternalFeedback.SensorToMechanismRatio = 1.0;
+    wristConfigs.SoftwareLimitSwitch.ReverseSoftLimitThreshold = -0.1;
+    wristConfigs.MotorOutput.NeutralMode = NeutralModeValue.Brake;
+
+    wristConfigurator.apply(wristConfigs);
+
+    // Shuffleboard grouping
     ShuffleboardTab tab = Shuffleboard.getTab("Arms");
     tab.add("Wrist", this);
-
   }
 
   public void setBrakeMode(NeutralModeValue mode) {
-   
-    wrist.getConfigurator().refresh(wristConfigs); // Load current config
+    wrist.getConfigurator().refresh(wristConfigs);
     wristConfigs.MotorOutput.NeutralMode = mode;
     wrist.getConfigurator().apply(wristConfigs);
-    }
+  }
+
   public void setPos(double position) {
-
-    wrist.setControl(wristController.withPosition(position)); // wristController = new PositionDutyCycle(position);
+    wrist.setControl(motionMagicRequest.withPosition(position));
     requestedPosition = position;
-
   }
 
   public void setSpeed(double speed) {
-    wrist.set(speed);
+    double limitedSpeed = MathUtil.clamp(speed, -0.2, 0.2);
+    wrist.set(limitedSpeed);
   }
 
   public double getPos() {
-    var pos = wrist.getPosition();
-    return wrist.getPosition().getValueAsDouble();
+    return cachedPos;
   }
 
   public boolean atPos() {
     return atPosition;
   }
 
-  public void moveIt(double position) {
-    newPosition = getPos() + position;
-    SmartDashboard.putNumber("WcurrentPOS", getPos());
-    SmartDashboard.putNumber("Wposition", requestedPosition);
+  public void moveIt(double positionOffset) {
+    newPosition = getPos() + positionOffset;
     setPos(newPosition);
-   
-
   }
 
   @Override
   public void periodic() {
-
-    if (Math.abs(Math.abs(getPos()) - Math.abs(requestedPosition)) < 0.3) {
-      atPosition = true;
-    } else {
-      atPosition = false;
-
-    }
-   
+    cachedPos = wrist.getPosition().getValueAsDouble();
+    atPosition = Math.abs(cachedPos - requestedPosition) < 0.3;
   }
 
   @Override
   public void initSendable(SendableBuilder builder) {
     builder.setSmartDashboardType("Wrist");
-    // builder.addDoubleProperty("Velocity RPM", () ->
-    // wrist.getVelocity().getValueAsDouble() * 60, null);
-    builder.addDoubleProperty("Position", () -> wrist.getPosition().getValueAsDouble(), null);
+
+    builder.addDoubleProperty("Position", this::getPos, null);
     builder.addDoubleProperty("Setpoint", () -> requestedPosition, this::setPos);
-    // builder.addDoubleProperty("Output Voltage", () ->
-    // wrist.getMotorVoltage().getValueAsDouble(), null);
-    // PID Tuning
+
     builder.addDoubleProperty("kP", () -> pidConfigs.kP, (val) -> {
-      pidConfigs.kP = val;
-      wrist.getConfigurator().apply(pidConfigs);
+      if (pidConfigs.kP != val) {
+        pidConfigs.kP = val;
+        wrist.getConfigurator().apply(pidConfigs);
+      }
     });
     builder.addDoubleProperty("kI", () -> pidConfigs.kI, (val) -> {
-      pidConfigs.kI = val;
-      wrist.getConfigurator().apply(pidConfigs);
+      if (pidConfigs.kI != val) {
+        pidConfigs.kI = val;
+        wrist.getConfigurator().apply(pidConfigs);
+      }
     });
     builder.addDoubleProperty("kD", () -> pidConfigs.kD, (val) -> {
-      pidConfigs.kD = val;
-      wrist.getConfigurator().apply(pidConfigs);
+      if (pidConfigs.kD != val) {
+        pidConfigs.kD = val;
+        wrist.getConfigurator().apply(pidConfigs);
+      }
     });
     builder.addDoubleProperty("kF", () -> pidConfigs.kV, (val) -> {
-      pidConfigs.kV = val;
-      wrist.getConfigurator().apply(pidConfigs);
+      if (pidConfigs.kV != val) {
+        pidConfigs.kV = val;
+        wrist.getConfigurator().apply(pidConfigs);
+      }
     });
   }
 }
